cd18064a1d4d698c739632f4eadcadf357a5f9a1
Gavin914 <zengjie.tang@gmail.com>
2024-03-17 23:29:30 +0800

    11

--- MP8_PartC.py
+++ MP8_PartC.py
@@ -40,8 +40,7 @@ df.createOrReplaceTempView("gbooks")
 # |      11|
 # +--------+
 
-df = df.filter(df["_1"] == 'ATTRIBUTE').collect()
-df.createOrReplaceTempView("gbooks")
+df = df.filter(df['`_1`'] == 'ATTRIBUTE').collect()
 
 spark.sql("SELECT COUNT(*) FROM gbooks").show()
 
